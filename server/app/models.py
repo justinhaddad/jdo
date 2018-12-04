1e33a13df89df1a9ce9c42920f5e71d1040fc0b7
jhaddad <jhaddad@arbor.net>
2018-12-04 15:26:44 -0500

    Added falcon api for creating and getting todos.  Using peewee to perist in sqlite.

---
+++ server/app/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime as dt
+from peewee import *
+
+db = SqliteDatabase('/var/lib/jdo.db')
+
+
+def connect():
+    db.connect()
+    db.create_tables([Todo])
+
+
+class Todo(Model):
+    headline = CharField()
+    created = DateField(default=dt.now())
+    next_reminder = DateField(null=True)
+    note = CharField(null=True)
+    repeat = CharField(null=True)
+    complete = BooleanField(default=False)
+    priority = IntegerField(null=True)
+
+    class Meta:
+        database = db
